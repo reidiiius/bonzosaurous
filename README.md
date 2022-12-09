63e0ed36124c5f40967d2e7a21fb0ce7239b2ce2
Reid Netterville III <reidiiius@gmail.com>
2022-12-08 21:22:38 -0600

    1670556158

--- README.md
+++ README.md
@@ -12,23 +12,23 @@ Paleontology
 ### Usage
 Command-line
 
-    cd bonzosaurous/ 
+    cd bonzosaurous/
 
 :
 
-    python3 -B laurel.py 
+    python3 -B laurel.py
 
 :
 
-    python3 -B laurel.py n0 j3 
+    python3 -B laurel.py n0 j3
 
 :
 
-    python3 -B laurel.py cgdae j3 j36 j236 
+    python3 -B laurel.py cgdae j3 j36 j236
 
 :
 
-    python3 -B laurel.py eadgbe gamut 
+    python3 -B laurel.py eadgbe gamut | sensible-pager
 
 ---
 
--- laurel.py
+++ laurel.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
 from phaedriades import omphalos
 from parnassus import *
@@ -11,76 +11,76 @@ lyra = beadgcf
 
 
 def fabricate():
-  for sign in args:
-    if omphalos.get(sign):
-      print('')
-      lyra(sign)
-    else:
-      print("\n\t{} ?".format(sign))
-  print('')
+    for sign in args:
+        if omphalos.get(sign):
+            print('')
+            lyra(sign)
+        else:
+            print("\n\t{} ?".format(sign))
+    print('')
 
 
 def chalkboard():
-  accum = str()
-  count = 0
-  for sign in sorted(omphalos.keys()):
-    accum = accum + "\t{}".format(sign)
-    count = count + 1
-    if count % 7 == 0:
-      accum = accum + "\n"
+    accum = str()
+    count = 0
+    for sign in sorted(omphalos.keys()):
+        accum = accum + "\t{}".format(sign)
+        count = count + 1
+        if count % 7 == 0:
+            accum = accum + "\n"
 
-  print("\n{}\n".format(accum))
-  print("Tunings:")
-  print("\tbeadgcf bfbfb cgdae eadgbe fkbjdn\n")
-  print("Example:")
-  print("\tpython3 -B {} eadgbe n0 k6 j3 j6 j2\n".format(args[0]))
+    print("\n{}\n".format(accum))
+    print("Tunings:")
+    print("\tbeadgcf bfbfb cgdae eadgbe fkbjdn\n")
+    print("Example:")
+    print("\tpython3 -B {} eadgbe n0 k6 j3 j6 j2\n".format(args[0]))
 
 
 if len(args) > 1:
-  dump = args.pop(0)
-  head = args[0]
+    dump = args.pop(0)
+    head = args[0]
 
-  if 'a4' in head:
-    lyra = bfbfb
-    dump = args.pop(0)
-  elif 'adgb' in head:
-    lyra = eadgbe
-    dump = args.pop(0)
-  elif 'bfb' in head:
-    lyra = bfbfb
-    dump = args.pop(0)
-  elif 'cgda' in head:
-    lyra = cgdae
-    dump = args.pop(0)
-  elif 'eadg' in head:
-    lyra = beadgcf
-    dump = args.pop(0)
-  elif 'gdae' in head:
-    lyra = cgdae
-    dump = args.pop(0)
-  elif 'fkbj' in head:
-    lyra = fkbjdn
-    dump = args.pop(0)
-  elif 'p4' in head:
-    lyra = beadgcf
-    dump = args.pop(0)
-  elif 'p5' in head:
-    lyra = cgdae
-    dump = args.pop(0)
-  elif 'm3' in head:
-    lyra = fkbjdn
-    dump = args.pop(0)
-  else:
-    lyra = beadgcf
+    if 'a4' in head:
+        lyra = bfbfb
+        dump = args.pop(0)
+    elif 'adgb' in head:
+        lyra = eadgbe
+        dump = args.pop(0)
+    elif 'bfb' in head:
+        lyra = bfbfb
+        dump = args.pop(0)
+    elif 'cgda' in head:
+        lyra = cgdae
+        dump = args.pop(0)
+    elif 'eadg' in head:
+        lyra = beadgcf
+        dump = args.pop(0)
+    elif 'gdae' in head:
+        lyra = cgdae
+        dump = args.pop(0)
+    elif 'fkbj' in head:
+        lyra = fkbjdn
+        dump = args.pop(0)
+    elif 'p4' in head:
+        lyra = beadgcf
+        dump = args.pop(0)
+    elif 'p5' in head:
+        lyra = cgdae
+        dump = args.pop(0)
+    elif 'm3' in head:
+        lyra = fkbjdn
+        dump = args.pop(0)
+    else:
+        lyra = beadgcf
 
-  if callable(lyra) and not len(args):
-      print("\n\t {} [key [key [...]]]\n".format(dump))
-  elif callable(lyra) and args[0] == 'gamut':
-    pleistos(lyra)
-  else:
-    fabricate()
+    if callable(lyra) and not len(args):
+        print("\n\t {} [key [key [...]]]\n".format(dump))
+    elif callable(lyra) and args[0] == 'gamut':
+        pleistos(lyra)
+    else:
+        fabricate()
 
 else:
-  chalkboard()
+    chalkboard()
 
 
